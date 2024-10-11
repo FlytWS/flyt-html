464a9463a8552ae8a5c52eedd88228b4cab0442c
FlytWS <flytws@outlook.com>
2024-10-11 21:07:07 +0100

    2410112107

--- gnss/ajax.php
+++ gnss/ajax.php
@@ -1,21 +1,10 @@
 <?php
 clearstatcache();
-	
-
-
-	if ($_POST['request'] == "get-flyt-model") {
 		
-		$file = '/etc/flyt/data/flyt-gnss.json';
-		if (file_exists($file)) {
-			echo file_get_contents($file);
-		};
-		
-	}
-	
 	
 	if ($_POST['request'] == "get-flyt-gnss") {
 		
-		$file = '/etc/flyt/data/flyt-stats-1.json';
+		$file = '/etc/flyt/data/flyt-gnss.json';
 		if (file_exists($file)) {
 			echo file_get_contents($file);
 		};
