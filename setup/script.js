38080f7f9b98f8109b6fb50df2c54e2ba3a7af38
FlytWS <flytws@outlook.com>
2024-10-15 13:07:27 +0100

    2410151307

--- setup/script.js
+++ setup/script.js
@@ -108,7 +108,8 @@ function WiFiConnectBtn() {
 
 var wifirescanrotate = 0;
 function WiFiRescan() {
-	wifiScan();
+	
+	document.getElementById('lets-go-div').innerHTML = '<svg class="load" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12a9 9 0 1 1-6.219-8.56"></path></svg>';
 	
 	wifirescanrotate += 360;
 	
@@ -121,6 +122,8 @@ function WiFiRescan() {
 		}
 	  }
 	);
+	
+	wifiScan();	
 
 }
 
@@ -579,7 +582,7 @@ function getGNSSLocation(tag) {
 
 
 function wifiScan() {
-
+	
 	$.ajax({
 	url: 'ajax.php',
 	type: 'POST',
@@ -611,7 +614,20 @@ function wifiScan() {
 
 var wifimanagerotate = 0;
 function wifiManage() {
+	
+	document.getElementById('lets-go-div').innerHTML = '<svg class="load" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12a9 9 0 1 1-6.219-8.56"></path></svg>';
 
+	wifimanagerotate += 360;
+	$('#wifiManage').stop().animate(
+	  {rotation: wifimanagerotate},
+	  {
+		duration: 500,
+		step: function(now, fx) {
+		  $(this).css({"transform": "rotate("+now+"deg)"});
+		}
+	  }
+	);	
+	
 	$.ajax({
 	url: 'ajax.php',
 	type: 'POST',
@@ -623,17 +639,6 @@ function wifiManage() {
 		console.log(response);
 		var resParse = JSON.parse(response);
 		console.log(resParse);
-		
-		wifimanagerotate += 360;
-		$('#wifiManage').stop().animate(
-		  {rotation: wifimanagerotate},
-		  {
-			duration: 500,
-			step: function(now, fx) {
-			  $(this).css({"transform": "rotate("+now+"deg)"});
-			}
-		  }
-		);
 
 
 		var ssidContent = "";
