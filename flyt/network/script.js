f5ee49cf4181859a4c2ca553ab7f4bf1e0f93af2
FlytWS <flytws@outlook.com>
2024-10-20 23:01:53 +0100

    2410202301

--- flyt/network/script.js
+++ flyt/network/script.js
@@ -283,6 +283,25 @@ function secondsToDHM(seconds) {
 	var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes ") : "";
 	return dDisplay + hDisplay + mDisplay;
 }
+function timeConverter(UNIX_timestamp){
+	var a = new Date(UNIX_timestamp * 1000);
+	var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+	var year = a.getFullYear();
+	var month = months[a.getMonth()];
+	var date = a.getDate();
+	//var hour = a.getHours();
+	//var min = a.getMinutes();
+	//var sec = a.getSeconds();
+	var time = date + ' ' + month + ' ' + year;
+	return time;
+}
+function timeConverterHMS(UNIX_timestamp){
+	var a = new Date(UNIX_timestamp * 1000);
+	var hour = a.getHours();
+	var min = a.getMinutes();
+	var time = hour + ' : ' + min;
+	return time;
+}
 
 
 
