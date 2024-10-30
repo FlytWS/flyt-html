975d37e18fd42208d617de3a35c5dba2010b8f34
FlytWS <flytws@outlook.com>
2024-10-30 14:28:02 +0000

    2410301428

--- flyt/map/script.js
+++ flyt/map/script.js
@@ -215,16 +215,12 @@ function setDisplayMode() {
 	if (localStorage.displayModeFlag == 1) {
 		displayModeFlag = 1;
 		$('.map-tiles').css({"filter" : "brightness(0.6) invert(1) contrast(3) hue-rotate(200deg) saturate(0.3) brightness(0.7)"});
-		document.getElementById('display-control-menu-item-mode').innerHTML = '<svg class="standard-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>';
-		document.getElementById('nav-mobile-burger-svg').style.color = "white";
-		document.getElementById('nav-mobile-burder-page-title').style.color = "white";
+
 
 	} else {
 		displayModeFlag = 0;
 		$('.map-tiles').css({"filter" : "none"});
-		document.getElementById('display-control-menu-item-mode').innerHTML = '<svg class="standard-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>';
-		document.getElementById('nav-mobile-burger-svg').style.color = "black";
-		document.getElementById('nav-mobile-burder-page-title').style.color = "black";
+
 		
 	}
 
