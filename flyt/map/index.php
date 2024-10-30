bc92935c448f96fdc96a5e18949bfabcde12454a
FlytWS <flytws@outlook.com>
2024-10-30 22:38:50 +0000

    2410302238

--- flyt/map/index.php
+++ flyt/map/index.php
@@ -66,7 +66,6 @@
 	
 	    <section id="page-hero" class="flex justify-space-between">
 		<div class="display-craft-header-action-topfade"></div>
-		<div class="display-craft-header-action-bottomfade"></div>
 			<div class="header-action display-craft-header-action">
 				<svg class="standard-icon" onclick="mapCraftClose();" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg>
 			</div>
@@ -501,7 +500,7 @@
 		<section>
 		<div id="display-control-altitude"></div>
 		<div id="display-control-altitude-reference-pad">
-		<div id="display-control-altitude-reference">O</div>
+		<div id="display-control-altitude-reference"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-dot"><circle cx="12" cy="12" r="10"/><circle cx="12" cy="12" r="1"/></svg></div>
 		</div>
 		<div id="display-control-altitude-0">0 ft</div>
 		<div id="display-control-altitude-25">10,000 ft</div>
