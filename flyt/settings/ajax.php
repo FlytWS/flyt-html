82ae486edfbc7710a0cd9dd7f8416c1052e75b54
FlytWS <flytws@outlook.com>
2024-11-01 11:41:28 +0000

    2411011141

--- flyt/settings/ajax.php
+++ flyt/settings/ajax.php
@@ -1,7 +1,54 @@
 <?php
 clearstatcache();
 	
+
+
+
+// Location
+
+
+	if ($_POST['request'] == "get-location") {
+		
+		$file = '/etc/flyt/data/flyt-location.json';
+		if (file_exists($file)) {
+			echo file_get_contents($file);
+		};
+		
+	}
 	
+	
+	if ($_POST['request'] == "get-gnss") {
+		
+		$file = '/etc/flyt/data/flyt-gnss.json';
+		if (file_exists($file)) {
+			echo file_get_contents($file);
+		};
+		
+	}
+	
+	if ($_POST['request'] == "set-location") {
+		
+		$latitude = $_POST['latitude'];
+		$longitude = $_POST['longitude'];
+		
+		
+		$filename = "/etc/flyt/data/flyt-location.json";
+		$file = fopen($filename, "w+") or die("Unable to open file!");
+		$text = '{"latitude":"'.$latitude.'","longitude":"'.$longitude.'"}';
+		fwrite($file, $text);
+		fclose($file);
+
+
+		//exec("echo '{".$latitude.",".$longitude."}' | tee /etc/flyt/data/location.json", $output, $retval);
+		#exec("bash /etc/flyt/scripts/set-readsb-location.sh ".$obj->{'lat'}." ".$obj->{'lon'}."", $output, $retval);
+
+		$file = '/etc/flyt/data/flyt-location.json';
+		if (file_exists($file)) {
+			echo file_get_contents($file);
+		}
+		
+	}
+		
 	
 	
 ?>
--- flyt/settings/location.php
+++ flyt/settings/location.php
@@ -2,7 +2,7 @@
         <header>
 		  <div id="page-title-left"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-table-of-contents"><path d="M16 12H3"/><path d="M16 18H3"/><path d="M16 6H3"/><path d="M21 12h.01"/><path d="M21 18h.01"/><path d="M21 6h.01"/></svg></div>         
 		  <div id="page-title-center"><h1>Location</h1></div>
-		  <div id="page-title-right" onclick="menumanual('location')" class="slideout-manual-toggle"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#fbfbfb" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></div>
+		  <div id="page-title-right" onclick="menumanual('sm-location')" class="slideout-manual-toggle"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#fbfbfb" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></div>
         </header>
 		<div class="menuBoundry">
 		<section id="menu-body">
