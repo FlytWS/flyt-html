70a89d6d8e585f9e3f865784ce1d3b6e2f0f4950
FlytWS <flytws@outlook.com>
2024-11-01 16:26:35 +0000

    2411011626

--- flyt/settings/release.php
+++ flyt/settings/release.php
@@ -1,8 +1,8 @@
-<div class="slideout-manual" id="sm-node">
+<div class="slideout-manual" id="sm-release">
         <header>
 		  <div id="page-title-left"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bolt"><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"/><circle cx="12" cy="12" r="4"/></svg></div>         
-		  <div id="page-title-center"><h1>Node</h1></div>
-		  <div id="page-title-right" onclick="menumanual('sm-node')" class="slideout-manual-toggle"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#fbfbfb" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></div>
+		  <div id="page-title-center"><h1>Release</h1></div>
+		  <div id="page-title-right" onclick="menumanual('sm-release')" class="slideout-manual-toggle"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#fbfbfb" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></div>
         </header>
 		<div class="menuBoundry">
 		<section id="slideout-body" style="width:100%;">
