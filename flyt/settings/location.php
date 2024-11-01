c2978e8084132ee7187be0944065881fa7d3ecb4
FlytWS <flytws@outlook.com>
2024-11-01 12:43:56 +0000

    2411011243

--- flyt/settings/location.php
+++ flyt/settings/location.php
@@ -14,8 +14,8 @@
 					<br>Please ensure your GPS reciever is connected and has clear visibility of the sky. An approximate GPS position of your node will be displayed below. Please confirm your nodes exact location by selecting it on the map, to better assist with mapping.
 				</div>
 				
-				<div class="page-title-center"  style="flex-grow:1; margin-bottom:2rem; color:#fbfbfb80; text-align:center; font-size:0.75rem;">
-					<header style="margin-top:1rem; border-radius:0.75rem; flex-direction: column; padding:0rem; overflow:hidden; ">
+				<div class="page-title-center"  style="width: 100%; flex-grow:1; margin-bottom:2rem; color:#fbfbfb80; text-align:center; font-size:0.75rem;">
+					<header style="    height: 100%;    width: 100%; margin-top:1rem; border-radius:0.75rem; flex-direction: column; padding:0rem; overflow:hidden; ">
 					<div id="cMap"></div>
 					</header>
 				</div>
