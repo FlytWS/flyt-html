7c2c2275171f475f7a9556aaf0671d4fe4a3d2bd
FlytWS <flytws@outlook.com>
2024-11-04 23:00:20 +0000

    2411042300

--- flyt/settings/terminal.php
+++ flyt/settings/terminal.php
@@ -5,16 +5,23 @@
 		  <div id="page-title-right" onclick="menumanual('sm-terminal')" class="slideout-manual-toggle"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#fbfbfb" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></div>
         </header>
 		<div class="menuBoundry">
-		<section id="slideout-body" style="width:100%;">
+		
+		
+		
+		<section id="menu-body">
 				
 
 			<header style="flex-direction: column;">
-
 			
 				<div class="page-title"  style="display: flex; width:100%; margin-bottom:2rem; color:#fbfbfb80; font-size:0.75rem;">
 					<div style="font-weight:600; white-space: nowrap;">Terminal</div>&emsp;<div style="width: calc(100% - 6rem);    word-wrap: break-word;    display: inline-block;">Use with caution</div>
 				</div>
-
+				
+				<div class="page-title-center" style="width: 100%; flex-grow:1; margin-bottom:2rem; color:#fbfbfb80; text-align:center; font-size:0.75rem;">
+					<header style="    height: 100%;    width: 100%; margin-top:1rem; border-radius:0.75rem; flex-direction: column; padding:0rem; overflow:hidden; ">
+					
+					
+					
 				<div id="terminal">
 					<div id="terminalhistory"></div>
 
@@ -22,12 +29,41 @@
 
 					<div id="terminalinput" contenteditable="true"></div><button id="terminalcaret" for="terminalinput">&nbsp;</button>
 				</div>
-
+				
+				
+					</header>
+				</div>
 
 			</header>
 
 	
 		</section>
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
 			
 		</div>
 </div>
@@ -48,6 +84,7 @@
   word-break: break-all;
   margin: 0;
   padding: 16px;
+  width:100%;
 }
 
 #terminalinput {
